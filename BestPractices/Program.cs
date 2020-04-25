b2f47f9f0e23ece4926f523b62999f6b2cd9ca51
Edmore Tshuma <tshumae@fbc.corp>
2020-04-25 13:52:35 +0200

    Initial commit

---
+++ BestPractices/Program.cs
@@ -0,0 +1,20 @@
+﻿using BestPractices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentActor = new Actor();
+
+            currentActor.GetOccupation();
+
+        }
+    }
+}
