95766def415069180d0303d5872e42ac3ea80e74
Edmore Tshuma <tshumae@fbc.corp>
2020-05-07 06:05:40 +0200

    Static class and static members

---
+++ BestPractices.Common/NotificationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BestPractices.Common
+{
+    static public class NotificationService
+    {
+        static public string NotifyTalent(string talentName)
+        {
+            var message = "talent" + talentName;
+            Console.WriteLine(message);
+            return message;
+        }
+    }
+}
