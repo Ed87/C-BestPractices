92f91c77df17b1dacb39e4c4def8dc36e28d2c1d
Edmore Tshuma <tshumae@fbc.corp>
2020-05-07 05:18:52 +0200

    Unit test GetOccupation

---
+++ TestsProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestsProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestsProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc12ec8b-eb87-442c-8188-f3bbb0896325")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestsProject/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BestPractices.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsProject
+{
+    [TestClass]
+    public class ActorTest
+    {
+        [TestMethod]
+        public void TestGetOccupation()
+        {
+            //Arrange
+            var currentActor = new Actor();
+            var expected = "Actor";
+
+            //Act
+            string result = currentActor.GetOccupation();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
