bc590af80c04a31f916226500439e36d4af95a61
Edmore Tshuma <tshumae@fbc.corp>
2020-05-07 05:41:31 +0200

    Unit test constructor

--- TestsProject/UnitTest1.cs
+++ TestsProject/UnitTest1.cs
@@ -20,5 +20,19 @@ namespace TestsProject
             //Assert
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        public void TestParameterizedConstructor()
+        {
+            //Arrange
+            var currentActor = new Actor("Ed Tshuma");
+            var expected = "Ed Tshuma";
+
+            //Act
+            string result = currentActor.JobTitle;
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }
